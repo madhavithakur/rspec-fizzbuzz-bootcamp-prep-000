8ad68636c9690a01848aa5979fcab214d059a1c4
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-08 05:29:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,8 +8,8 @@ def fizzbuzz(num)
     return "Fizz"
   elsif num % 5 == 0 
     return "Buzz"
-  elsif (num % 3 == 0 && num % 5 == 0)
-    return "FizzBuzz"
+  # elsif (num % 3 == 0 && num % 5 == 0)
+  #   return "FizzBuzz"
   else
     nil
   end 
